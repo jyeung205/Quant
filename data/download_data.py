e2376662d1e90d98edeadd42e153459960307372
Jimmy Yeung <53492347+jyeung205@users.noreply.github.com>
2023-01-13 13:07:50 +0000

    amended download data function

--- data/download_data.py
+++ data/download_data.py
@@ -1,32 +1,26 @@
-
 import requests
 import bs4 as bs
 import pickle
-import os
-import pandas_datareader.data as web
 import datetime
 import pandas as pd
 import sqlite3
-import yfinance as yf
+
+from alpha_vantage.timeseries import TimeSeries
 
 
 class Data:
-    yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
-    yesterday = yesterday.strftime('%Y-%m-%d')
+    yesterday = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 
-    def save_index_tickers(self, index):
+    def save_index_tickers(self, index='S&P 100'):
         """
         This function saves the list of S&P 500 tickers in a .pickle file
         """
         if index == "S&P 500":
             resp = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-
         elif index == "S&P 100":
             resp = requests.get("https://en.wikipedia.org/wiki/S%26P_100")
-
         elif index == "NASDAQ 100":
             resp = requests.get("https://en.wikipedia.org/wiki/NASDAQ-100")
-
         else:
             print('Input valid index!!!')
             return
@@ -41,8 +35,7 @@ class Data:
                 ticker = row.findAll("td")[1].text
             tickers.append(ticker)
 
-        # Remove \n from each ticker
-        tickers = [ticker[:-1] for ticker in tickers]
+        tickers = [ticker[:-1] for ticker in tickers]  # Remove \n from each ticker
 
         if index == "S&P500":
             with open("tickers/sp500tickers.pickle", "wb") as f:
@@ -57,53 +50,48 @@ class Data:
         print(tickers)
         return tickers
 
-    def download_data(self, tickers, start="2000-01-01", end=yesterday):
-        """
-        Will add a repeated date if this function is executed on a non-trading day.
-        """
+    def get_stock_data(self, ticker, start, end):
+        app = TimeSeries("LHE90GZROAECNZ96", output_format='pandas')  # TODO add alpha vantagae api key to env variables
+        df = app.get_daily_adjusted(ticker, 'full')[0]
+        df.sort_index(inplace=True)
+        new_df = pd.concat([df['2. high'][start:end],
+                            df['3. low'][start:end],
+                            df['1. open'][start:end],
+                            df['4. close'][start:end],
+                            df['6. volume'][start:end],
+                            df['5. adjusted close'][start:end]], axis=1)
+        new_df.columns = ['High', 'Low', 'Open', 'Close', 'Volume', 'Adj Close']
+        new_df.index = new_df.index.date
+        new_df.index.name = "Date"
+        return new_df
 
+    def download_many_stock_data(self, tickers, start="2000-01-01", end=yesterday):
+        """
+        Only run on trading days. Function will add a repeated date if executed on a non-trading day.
+        """
         conn = sqlite3.connect("stock_data.db")
         c = conn.cursor()
         no_data_tickers = []
 
-        if type(tickers) == str:
+        if type(tickers) == str:  # if input is str and not a list
             tickers = [tickers]
 
         for ticker in tickers:
             c.execute(f"SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{ticker}'")
             if c.fetchone()[0] == 1:  # If table exists
                 for date in conn.execute(f"SELECT DATE FROM '{ticker}' ORDER BY DATE DESC LIMIT 1"):  # get last date
-                    # print(date[0])
-                    # last_date = datetime.datetime.strptime(date[0], "%Y-%m-%d")
-                    if date[0] == self.yesterday:
+                    if date[0] == end:
                         print(f"{ticker} is up to date")
                     else:
-                        last_date_next = datetime.datetime.strptime(date[0], "%Y-%m-%d") + datetime.timedelta(days=1)
-                        last_date_next = last_date_next.strftime("%Y-%m-%d")
-                        try:
-                            print(f"Downloading Data for {ticker} from {last_date_next} to {self.yesterday}")
-                            # df = web.DataReader(ticker, "yahoo", start=last_date_next, end=self.yesterday)
-                            df = yf.Ticker(ticker, start=last_date_next, end=self.yesterday)[["High", "Low", "Open", "Close", "Volume", "Adj Close"]]
-                            df.index = df.index.date
-                            df.index.name = "Date"
-                            df.to_sql(ticker, conn, schema=None, if_exists="append")
-                        except:
-                            print("No Data for " + ticker + f"from {last_date_next} to {self.yesterday}")
+                        next_date_in_db = datetime.datetime.strptime(date[0], "%Y-%m-%d") + datetime.timedelta(days=1)
+                        next_date_in_db = next_date_in_db.strftime("%Y-%m-%d")
+                        print(f"Downloading Data for {ticker} from {next_date_in_db} to {end}")
+                        df = self.get_stock_data(ticker, next_date_in_db, end)
+                        df.to_sql(ticker, conn, schema=None, if_exists="append")
             else:  # If table does not exist
-                # try:
-                df = web.DataReader(ticker, "yahoo", start, end)
+                df = self.get_stock_data(ticker, next_date_in_db, end)
                 print(f"Downloading Data for {ticker} from {start} to {end}")
-                # except :
-                # print("No Data for " + ticker)
-                no_data_tickers.append(ticker)
-
-                df.index = pd.to_datetime(df.index)
-                df.index = df.index.date
-                df.index.name = "Date"
                 df.to_sql(ticker, conn, schema=None, if_exists="replace")
-
-        if len(no_data_tickers) != 0:
-            print("Finished!. No Data for", no_data_tickers)
 
     def delete_latest_date(self):
         """
@@ -118,9 +106,6 @@ class Data:
 
 
 if __name__ == "__main__":
-    # data = Data()
-    # tickers = data.save_index_tickers("S&P 500")
-    # data.download_data(["AAPL"])
-    # df = web.DataReader("AAPL", "yahoo")
-    aapl = yf.Ticker('AAPL')
-    print(aapl.history()[["High", "Low", "Open", "Close", "Volume"]])
+    data = Data()
+    data.download_many_stock_data(['AAL'])
+
